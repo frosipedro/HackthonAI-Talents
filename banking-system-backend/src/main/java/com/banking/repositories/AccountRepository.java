2c70a68147c2e7b2ebbe1031f02603fe25a805e8
Luan Gerlach <luancgerlach@gmail.com>
2025-04-29 22:13:41 -0300

    feature: config customer for cascade delete accounts

--- banking-system-backend/src/main/java/com/banking/repositories/AccountRepository.java
+++ banking-system-backend/src/main/java/com/banking/repositories/AccountRepository.java
@@ -3,8 +3,10 @@ package com.banking.repositories;
 import com.banking.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByCustomerIdAndAccountType(Long customerId, String accountType);
+    List<Account> findByCustomerId(Long customerId);
 }
